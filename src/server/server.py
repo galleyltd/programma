995da96928ded61cfd347836d5c81351d33b300d
Igor Perikov <perikov.igor@gmail.com>
2018-03-27 21:12:30 +0300

    remove kafka, update web framework, split requirements

---
+++ src/server/server.py
@@ -0,0 +1,27 @@
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+
+class ErrorHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.set_header('Content-Type', 'application/json')
+        self.set_status(500)
+        self.write({"key": "value"})
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/error", ErrorHandler)
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
